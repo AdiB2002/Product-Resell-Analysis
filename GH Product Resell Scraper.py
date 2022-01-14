d23fd52d32f89f5896424151e0cde07744eef2de
AdiB2002 <66137006+AdiB2002@users.noreply.github.com>
2022-01-14 11:03:14 -0600

    Add files via upload

--- GH Product Resell Scraper.py
+++ GH Product Resell Scraper.py
@@ -11,7 +11,7 @@ import pandas as pd
 class Product():
     
     # constructor with default values
-    def __init__(self, name = 'N/A', price = 0, old_price = 0, website = 'N/A', ebay_price = 0, category = 'N/A', difference_price = 0, binary_answer = 0, manual_answer = 0):
+    def __init__(self, name = 'N/A', price = 0, old_price = 0, website = 'N/A', ebay_price = 0, category = 'N/A', difference_price = 0, binary_answer = 0, binary_answer_check = 0, feasability = 0):
         self.name = name
         self.price = price
         self.old_price = old_price
@@ -20,7 +20,8 @@ class Product():
         self.category = category
         self.difference_price = difference_price
         self.binary_answer = binary_answer
-        self.manual_answer = manual_answer
+        self.binary_answer_check = binary_answer_check
+        self.feasability = feasability
       
     # checks if deal news products are legit
     def dn_product_check(self):
@@ -42,7 +43,7 @@ class Product():
      
     # puts attributes in format to be exported to excel        
     def excel_format(self):
-        return [self.name, self.price, self.old_price, self.website, self.ebay_price, self.category, self.difference_price, self.binary_answer, self.manual_answer]
+        return [self.name, self.price, self.old_price, self.website, self.ebay_price, self.category, self.difference_price, self.binary_answer, self.binary_answer_check, self.feasability]
     
     # creates a print statement for Product object
     def __str__(self):
@@ -51,7 +52,7 @@ class Product():
 class Scraper():
     
     # class-wide variables no need for a constructor
-    # put chromedriver location in empty single quotes
+    # needs path of chromedriver
     executable_path = Service('')
     browser = webdriver.Chrome(service = executable_path)
     wait = WebDriverWait(browser, 5)
@@ -124,9 +125,14 @@ class Scraper():
             self.turn_list_to_text(websites)
             websites = [x.split()[0] for x in websites]
             
-            # accuracy test
+            # accuracy test for regular scrape
             if(scrape_extra == False):
                 self.dn_accuracy_check(products, prices, websites)
+            else:
+                
+                # accuracy test for scrape_extra = True because it's much simpler 
+                if(len(products) != len(prices) or len(prices) != len(websites) or len(websites) != len(products)):
+                    continue
                 
             # creates Product objects and adds to list if they are legit (uses method in Product class)
             for i in range(0,len(prices)):
@@ -135,7 +141,12 @@ class Scraper():
         
         # fixes the price 
         for Product_obj in self.product_list:
-            Product_obj.fix_dn_price()
+            
+            # tries to get price but very few prices don't meet formatting and these are unlikely to scrape on ebay anyways 
+            try:
+                Product_obj.fix_dn_price()
+            except:
+                self.product_list.remove(Product_obj)
       
     # scrapes ebay for products from deal news
     def scrape_ebay(self):
@@ -240,7 +251,7 @@ class Scraper():
     def export_to_excel(self, file = ''):
         
         # creates a dataframe out of scraped products
-        product_df = pd.DataFrame(columns = ['Name', 'Price', 'Old Price', 'Website', 'Ebay Price', 'Category', 'Difference Price', 'Binary Answer', 'Manual Answer'])
+        product_df = pd.DataFrame(columns = ['Name', 'Price', 'Old Price', 'Website', 'Ebay Price', 'Category', 'Difference Price', 'Binary Answer', 'Binary Answer Check', 'Feasability'])
         for Product_obj in self.product_list:
             product_df.loc[len(product_df.index)] = Product_obj.excel_format()
         
@@ -258,20 +269,31 @@ class Scraper():
         combined_product_df.to_excel(file, index = False) 
         print('\nSuccessfully exported to:', file)
     
-    # used to change the manual answer column
-    def change_manual_answer(self, file = ''):
+    # used to change the manual answer columns
+    def change_manual_answers(self, file = ''):
+        
+        # tries to close the browser if open
+        try:
+            self.browser.close()
+        except:
+            pass
         
         # reads in the current data
         current_product_df = pd.read_excel(file)
         
-        # asks for input for products and then checks binary answer is 1 before changing manual answer to 1
+        # asks for input for products and then checks binary answer is 1 before potentially changing manual answers 
         product_input = ''
         while(product_input.upper() != 'N'):
             product_input = input('Enter product or N to stop: ')
             row_index_list = current_product_df.index[current_product_df['Name'] == product_input].tolist()
             for row_index in row_index_list:
                 if(current_product_df.at[row_index, 'Binary Answer'] == 1):
-                    current_product_df.at[row_index, 'Manual Answer'] = 1
+                    if(input("Would you like to change Binary Answer Check? Enter Y or N: ").upper() == 'Y'): 
+                        current_product_df.at[row_index, 'Binary Answer Check'] = 1
+                        print('\nChanged Binary Answer Check for', product_input)
+                    if(input("Would you like to change Feasability? Enter Y or N: ").upper() == 'Y'): 
+                        current_product_df.at[row_index, 'Feasability'] = 1
+                        print('\nChanged Feasability for', product_input)
         
         # saves as an excel file
         current_product_df.to_excel(file, index = False) 
@@ -283,7 +305,7 @@ def main():
     # parameter is url and scroll length which can be set to 0 for around 50 deals or 5000-8000 for around 90 deals 
     # might need to fiddle with scroll if not set at 0 to get an accurate scrape
     # if scraping extra = True set scroll length to 0
-    Scraper_obj.scrape_deal_news(scrape_extra = False, scroll_length = 6000)
+    Scraper_obj.scrape_deal_news(scrape_extra = True, scroll_length = 0)
     
     # scrapes ebay no parameters needed
     Scraper_obj.scrape_ebay()
@@ -294,8 +316,8 @@ def main():
     # exports to excel and can take file as a parameter 
     Scraper_obj.export_to_excel()
     
-    # allows to change the manual answer column and can take file as a parameter
-    #Scraper_obj.change_manual_answer()
+    # allows to change the manual answer columns and can take file as a parameter
+    Scraper_obj.change_manual_answers()
          
 main()
   
