1cb646944629203f9c8451e553ed4cd624f477d2
AdiB2002 <66137006+AdiB2002@users.noreply.github.com>
2022-05-24 23:52:23 -0500

    Add files via upload

--- GH Product Resell Scraper.py
+++ GH Product Resell Scraper.py
@@ -1,5 +1,3 @@
-# Code by Aadit Barua
-
 from selenium import webdriver  
 from selenium.webdriver.chrome.service import Service                  
 from selenium.webdriver.common.by import By
@@ -52,11 +50,12 @@ class Product():
 class Scraper():
     
     # class-wide variables no need for a constructor
-    # needs path of chromedriver
-    executable_path = Service('')
+    executable_path = Service('') # put chromedriver path
     browser = webdriver.Chrome(service = executable_path)
     wait = WebDriverWait(browser, 5)
     product_list = []
+    ebay_error_list = []
+    deal_news_error_list = []
        
     # turns scraped web elements into a usable list 
     def turn_list_to_text(self, list_passed):
@@ -79,6 +78,8 @@ class Scraper():
                 if(remove_product.upper() == 'N'):
                     self.browser.close()
                     raise ValueError("Accuracy check didn't pass.")
+                
+                # product entered not in products causes error
                 try:
                     removal_index = products.index(remove_product)
                     del products[removal_index]
@@ -98,46 +99,57 @@ class Scraper():
         print()
         
     # scrapes the deal news website for deals
-    def scrape_deal_news(self, scrape_extra = False, scroll_length = 0):
+    def scrape_deal_news(self, scrape_extra = False):
         
         # list of deal news urls for other specific categories
         urls = ['https://www.dealnews.com/f1682/Staff-Pick/', 'https://www.dealnews.com/c202/Clothing-Accessories/', 'https://www.dealnews.com/c196/Home-Garden/', 'https://www.dealnews.com/c756/Health-Beauty/', 'https://www.dealnews.com/c142/Electronics/', 'https://www.dealnews.com/c39/Computers/', 'https://www.dealnews.com/c211/Sports-Fitness/', 'https://www.dealnews.com/c186/Gaming-Toys/', 'https://www.dealnews.com/c182/Office-School-Supplies/', 'https://www.dealnews.com/c238/Automotive/', 'https://www.dealnews.com/c178/Movies-Music-Books/']
         
-        # if scrape extra = False than just scrape the regular page 
+        # if scrape extra = True then scroll length defaults to 5000
+        scroll_length = 5000
+        
+        # if scrape extra = False then just scrape the regular page and set scroll length to 0
         if(scrape_extra == False):
+            scroll_length = 0
             urls = ['https://www.dealnews.com/']
         
         # loops through urls
         for url in urls:
-        
-            # opens a browser
-            self.browser.get(url) 
-            
-            # scrolls down the page to get some more deals (any larger of a number messes up the price scraping)
-            self.browser.execute_script("window.scrollTo(0, " + str(scroll_length) + ")")
-            
-            # scrapes prices, products, and websites (does a bit of filtering)          
-            prices = self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'callout.limit-height.limit-height-large-1.limit-height-small-1')))
-            self.turn_list_to_text(prices)
-            products = self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'title.limit-height.limit-height-large-2.limit-height-small-2')))
-            self.turn_list_to_text(products)
-            websites = self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'key-attribute.limit-height.limit-height-large-1.limit-height-small-1')))
-            self.turn_list_to_text(websites)
-            websites = [x.split()[0] for x in websites]
-            
-            # accuracy test for regular scrape
-            if(scrape_extra == False):
-                self.dn_accuracy_check(products, prices, websites)
-            else:
                 
-                # accuracy test for scrape_extra = True because it's much simpler 
-                if(len(products) != len(prices) or len(prices) != len(websites) or len(websites) != len(products)):
-                    continue
+            # error sometimes with tab crashing
+            try:
                 
-            # creates Product objects and adds to list if they are legit (uses method in Product class)
-            for i in range(0,len(prices)):
-                if(Product(name = products[i], price = prices[i], website = websites[i]).dn_product_check() == True):
-                    self.product_list.append(Product(name = products[i], price = prices[i], website = websites[i]))
+                # opens a browser
+                self.browser.get(url) 
+                
+                # scrolls down the page to get some more deals (any larger of a number messes up the price scraping)
+                self.browser.execute_script("window.scrollTo(0, " + str(scroll_length) + ")")
+            
+                # scrapes prices, products, and websites (does a bit of filtering)          
+                prices = self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'callout.limit-height.limit-height-large-1.limit-height-small-1')))
+                self.turn_list_to_text(prices)
+                products = self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'title.limit-height.limit-height-large-2.limit-height-small-2')))
+                self.turn_list_to_text(products)
+                websites = self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'key-attribute.limit-height.limit-height-large-1.limit-height-small-1')))
+                self.turn_list_to_text(websites)
+                websites = [x.split()[0] for x in websites]
+                
+                # accuracy test for regular scrape
+                if(scrape_extra == False):
+                    self.dn_accuracy_check(products, prices, websites)
+                else:
+                    # accuracy test for scrape_extra = True because it's much simpler 
+                    if(len(products) != len(prices) or len(prices) != len(websites) or len(websites) != len(products)):
+                        continue
+                    
+                # creates Product objects and adds to list if they are legit (uses method in Product class)
+                for i in range(0,len(prices)):
+                    if(Product(name = products[i], price = prices[i], website = websites[i]).dn_product_check() == True):
+                        self.product_list.append(Product(name = products[i], price = prices[i], website = websites[i]))
+            
+            # prints url along with error
+            except Exception as e:
+                print(url + '  ERROR\n\n')
+                self.deal_news_error_list.append(e)
         
         # fixes the price 
         for Product_obj in self.product_list:
@@ -151,86 +163,102 @@ class Scraper():
     # scrapes ebay for products from deal news
     def scrape_ebay(self):
         
-        # opens initial browser, starts count, and loops through Product object list
-        self.browser.get('https://www.ebay.com/') 
+        # opens initial browser and ensures it loads ebay because sometimes it gets invalid session ids
+        works = False
+        while(works == False):
+            try:
+                self.browser.get('https://www.ebay.com/') 
+                works = True
+            except:
+                pass
+        
+        # starts count and loops through Product object list
         count = 0
         for Product_obj in self.product_list:
-            count += 1
-            print(str(count) + '/' + str(len(self.product_list)))
             
-            # enters Product name into search and enters
-            self.browser.find_element(By.ID, "gh-ac").send_keys(Product_obj.name)
-            self.browser.find_element(By.ID, "gh-btn").click()
-            
-            # checks for the "no exact match found" and if it finds disregard product else keep going
+            # catches errors for cannot determine loading status from target frame detached, buy it now and free shipping options, category of products, lowest price for product and ignores those products
             try:
-                self.browser.find_element(By.CLASS_NAME, 'srp-save-null-search__heading')
-                self.browser.get('https://www.ebay.com/') 
-                continue
-            except:
-                pass
             
-            # clicks the new option but needs try except for special cases 
-            try:
-                self.browser.find_element(By.PARTIAL_LINK_TEXT, "New").click()
-            except:
-                pass
-            
-            # clicks buy it now and free shipping options
-            self.browser.find_element(By.PARTIAL_LINK_TEXT, "Buy It Now").click()
-            self.browser.find_element(By.LINK_TEXT, "Free Shipping").click()
-            
-            # scrapes ebay prices and names 
-            ebay_prices = self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 's-item__price')))
-            self.turn_list_to_text(ebay_prices)
-            ebay_names = self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 's-item__title')))
-            self.turn_list_to_text(ebay_names)
-            
-            # tries to scrape category of product but if it can't find disregard the product
-            try:
-                category = self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'srp-refine__category__item')))
-            except:
-                self.browser.get('https://www.ebay.com/') 
-                continue
-            self.turn_list_to_text(category)
-            Product_obj.category = category[1].split('\n')[0]
-            
-            # if scraped less than desired search length then make search len the scrape len
-            if(len(ebay_prices)<5):
-                search_len = len(ebay_prices)
-            else:
-                search_len = 5
-            
-            # tries to determine lowest price for product and fixes product variables 
-            try:
+                # increase count and print it
+                count += 1
+                print(str(count) + '/' + str(len(self.product_list)))
+                    
+                # enters Product name into search and enters could cause error 
+                self.browser.find_element(By.ID, "gh-ac").send_keys(Product_obj.name)
+                self.browser.find_element(By.ID, "gh-btn").click()
                 
-                # different Levenshtein standard for first item and if not met set high lowest price
+                # checks for the "no exact match found" and if it finds disregard product else keep going
+                try:
+                    self.browser.find_element(By.CLASS_NAME, 'srp-save-null-search__heading')
+                    self.browser.get('https://www.ebay.com/') 
+                    self.product_list.remove(Product_obj)
+                    continue
+                except:
+                    pass
+                
+                # clicks the new option but needs try except for special cases 
+                try:
+                    self.browser.find_element(By.PARTIAL_LINK_TEXT, "New").click()
+                except:
+                    pass
+                
+                # tries to click buy it now and free shipping options else likely not a good product so causes error
+                self.browser.find_element(By.PARTIAL_LINK_TEXT, "Buy It Now").click()
+                self.browser.find_element(By.LINK_TEXT, "Free Shipping").click()
+                    
+                # scrapes ebay prices and names 
+                ebay_prices = self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 's-item__price')))
+                self.turn_list_to_text(ebay_prices)
+                ebay_names = self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 's-item__title')))
+                self.turn_list_to_text(ebay_names)
+                
+                # tries to scrape category of product but if it can't find disregard the product because in later steps it will be removed
+                category = self.wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'srp-refine__category__item')))
+                self.turn_list_to_text(category)
+                Product_obj.category = category[1].split('\n')[0]
+                
+                # if scraped less than desired search length then make search len the scrape len
+                if(len(ebay_prices)<5):
+                    search_len = len(ebay_prices)
+                else:
+                    search_len = 5
+            
+                # tries to determine lowest price for product and fixes product variables and if error disregard
+                # different Levenshtein standard for first item found and if not met set arbitrarily high lowest price
                 if(Levenshtein.ratio(Product_obj.name, ebay_names[1]) > .35):
-                    lowest_price = float(ebay_prices[0].replace('$', '').split()[0])
+                    lowest_price = float(ebay_prices[1].replace('$', '').split()[0])
                 else:
                     lowest_price = 10000
-                    
+                   
                 # for all other checked ebay prices 
-                for i in range(1, search_len):
-                    if(Levenshtein.ratio(Product_obj.name, ebay_names[i+1]) > .6 and float(ebay_prices[i].replace('$', '').split()[0]) < lowest_price):
+                for i in range(2, search_len):
+                    if(Levenshtein.ratio(Product_obj.name, ebay_names[i]) > .6 and float(ebay_prices[i].replace('$', '').split()[0]) < lowest_price):
                         lowest_price = float(ebay_prices[i].replace('$', '').split()[0])
-            
-            # in case of error disregard product
-            except:
+                    
+                # fixes price and binary answer and if price is still arbitrarily high set it back to 0
+                if(lowest_price == 10000):
+                    lowest_price = 0
+                Product_obj.ebay_price = lowest_price
+                Product_obj.fix_diff_price_and_binary_ans()
+                
+                # resets browser for next product
                 self.browser.get('https://www.ebay.com/') 
+    
+            # print exceptions, remove product, and goes to next loop
+            except Exception as e: 
+                self.product_list.remove(Product_obj)
+                self.ebay_error_list.append(e)
+                print('ERROR')
                 continue
             
-            # fixes price and binary answer and if price is still arbitrarily high set it back to 0
-            if(lowest_price == 10000):
-                lowest_price = 0
-            Product_obj.ebay_price = lowest_price
-            Product_obj.fix_diff_price_and_binary_ans()
-            
-            # resets browser for next product
-            self.browser.get('https://www.ebay.com/') 
-        
         # closes ebay browser when done 
         self.browser.close()
+        
+        # export dataframe of errors from scraping 
+        error_df = pd.DataFrame(columns = ['Ebay Errors'])
+        error_df['Ebay Errors'] = self.ebay_error_list
+        error_df = pd.concat([error_df, pd.DataFrame({'Deal News Errors': self.deal_news_error_list})], axis = 1)
+        error_df.to_excel('', index = False) 
      
     # outputs results in a presentable manner
     def show_results(self):
@@ -250,24 +278,28 @@ class Scraper():
     # exports to excel         
     def export_to_excel(self, file = ''):
         
-        # creates a dataframe out of scraped products
-        product_df = pd.DataFrame(columns = ['Name', 'Price', 'Old Price', 'Website', 'Ebay Price', 'Category', 'Difference Price', 'Binary Answer', 'Binary Answer Check', 'Feasability'])
-        for Product_obj in self.product_list:
-            product_df.loc[len(product_df.index)] = Product_obj.excel_format()
+        # makes sure user is active when exporting so data isn't added with answers being manually checked
+        excel_input = input("Would you like to export to Excel? Y or N: ")
+        if(excel_input == 'Y'):
         
-        # tries to download current file and check for duplicate products
-        try:
-            current_product_df = pd.read_excel(file)
-            combined_product_df = current_product_df.append(product_df)
-            combined_product_df = combined_product_df.drop_duplicates(subset = ['Name', 'Price', 'Binary Answer'])
-            print('\n' + str(len(combined_product_df) - len(current_product_df)) + ' New Products Added')
-        except:
-            print("\nCouldn't Find File")
-            combined_product_df = product_df
-        
-        # saves as an excel file 
-        combined_product_df.to_excel(file, index = False) 
-        print('\nSuccessfully exported to:', file)
+            # creates a dataframe out of scraped products
+            product_df = pd.DataFrame(columns = ['Name', 'Price', 'Old Price', 'Website', 'Ebay Price', 'Category', 'Difference Price', 'Binary Answer', 'Binary Answer Check', 'Feasability'])
+            for Product_obj in self.product_list:
+                product_df.loc[len(product_df.index)] = Product_obj.excel_format()
+            
+            # tries to download current file and check for duplicate products
+            try:
+                current_product_df = pd.read_excel(file)
+                combined_product_df = current_product_df.append(product_df)
+                combined_product_df = combined_product_df.drop_duplicates(subset = ['Name', 'Price', 'Binary Answer'])
+                print('\n' + str(len(combined_product_df) - len(current_product_df)) + ' New Products Added')
+            except:
+                print("\nCouldn't Find File")
+                combined_product_df = product_df
+            
+            # saves as an excel file 
+            combined_product_df.to_excel(file, index = False) 
+            print('\nSuccessfully exported to:', file)
     
     # used to change the manual answer columns
     def change_manual_answers(self, file = ''):
@@ -300,12 +332,11 @@ class Scraper():
         print('\nSuccessfully exported to:', file)
         
 def main():
+    
     Scraper_obj = Scraper()
     
-    # parameter is url and scroll length which can be set to 0 for around 50 deals or 5000-8000 for around 90 deals 
-    # might need to fiddle with scroll if not set at 0 to get an accurate scrape
-    # if scraping extra = True set scroll length to 0
-    Scraper_obj.scrape_deal_news(scrape_extra = True, scroll_length = 0)
+    # parameter is scrape extra which can be set to True or False
+    Scraper_obj.scrape_deal_news(scrape_extra = True)
     
     # scrapes ebay no parameters needed
     Scraper_obj.scrape_ebay()
@@ -313,13 +344,13 @@ def main():
     # outputs scraped results 
     Scraper_obj.show_results()
     
-    # exports to excel and can take file as a parameter 
-    Scraper_obj.export_to_excel()
+    # prints how many errors during ebay scraping
+    print('\nThere were ' + str(len(Scraper_obj.ebay_error_list)) + ' errors during ebay scraping')
     
-    # allows to change the manual answer columns and can take file as a parameter
-    Scraper_obj.change_manual_answers()
+    # exports to excel and takes file as a parameter 
+    Scraper_obj.export_to_excel(file = '')
+    
+    # allows to change the manual answer columns and takes file as a parameter
+    Scraper_obj.change_manual_answers(file = '')
          
 main()
-  
-
-
